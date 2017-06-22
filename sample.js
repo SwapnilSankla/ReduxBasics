fa02095ff1b9c8fbe2bd59d2615bd0105395327e
Swapnil Sankla <sanklas@thoughtworks.com>
2017-06-22 20:13:08 +0530

    Displaying complete state after mutation

--- sample.js
+++ sample.js
@@ -5,6 +5,7 @@ seatMapStore.dispatch(action.updateNameAction("Swapnil"));
 seatMapStore.dispatch(action.updateRollNumberAction(2));
 console.log("RollNumber:", seatMapStore.getState().rollNumber);
 console.log("Name:", seatMapStore.getState().name);
+console.log("Before mutation State:", seatMapStore.getState());
 // Redux does not guarantee immutablility. Able to mutate name.
 seatMapStore.getState().name = 'sdfgh'
-console.log("After mutation Name:", seatMapStore.getState().name);
+console.log("After mutation State:", seatMapStore.getState());
